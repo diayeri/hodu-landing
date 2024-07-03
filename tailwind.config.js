91dec449f95a098d7c34e59bd32ddea03a4f885f
dia <jdyoung1031@gmail.com>
2024-07-03 10:46:20 +0900

    파일위치 변경, tailwind 세팅

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#D97652",
+        "primary-light": "#F29D52",
+        "bg-primary": "#F2E9D8",
+        "bg-secondary": "#263140",
+      },
+      fontSize: {
+        "4xl": "48px",
+        "3xl": "36px",
+        "2xl": "24px",
+        base: "16px",
+        sm: "14px",
+      },
+    },
+  },
+  plugins: [],
+};
